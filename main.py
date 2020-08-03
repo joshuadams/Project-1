4a1b66775a68c3029f68e28ba45595346b289dde
joshuadams <joshua.adams@warriorlife.net>
2020-08-03 19:47:09 +0000

    added if statment

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 for i in range(4):
-  print('Hello Cool World!')
+  print('Hello Cool World!')   
+  
+x = 14
+if x == 15:
+  print(x) 
+elif x ==14:
+  print(x)
+else:
+  print("It's not 15")
 
 
 
